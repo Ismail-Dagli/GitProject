ef1d08853a6a5246b0c7bbf45cf4ee839feae6c1
Puvude <dagli.i@icloud.com>
2024-12-26 21:00:11 +0100

    Here it is now!

--- mains.py
+++ mains.py
@@ -1,1 +1,18 @@
-print("Hello World")
+if __name__ == '__main__':
+    def calculate_square_root(x):
+        """
+        Calculate the square root of a number using the exponentiation operator.
+
+        This function calculates the square root of a given number by raising it
+        to the power of 0.5. The input number must be non-negative, as this
+        method does not support complex numbers.
+
+        :param x: A non-negative number for which the square root will be calculated.
+        :type x: float
+        :return: The square root of the input number.
+        :rtype: float
+        """
+        return x ** 0.5
+
+
+    print(calculate_square_root(25))
