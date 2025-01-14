b8c66720e809f4a8f6f9a9610748ee6034512d46
Puvude <dagli.i@icloud.com>
2025-01-14 13:25:58 +0100

    Where are we?

--- main.py
+++ main.py
@@ -1,23 +1,17 @@
-import numpy as np
-from itertools import combinations
+def find_prime_factors(n):
+    factors = []
+    for i in range(2, int(n ** 0.5) + 1):
+        while n % i == 0:
+            factors.append(i)
+            n //= i
 
-# Gegebene Prüfmatrix H
-H = np.array([
-    [1, 0, 0, 0, 1],
-    [0, 1, 0, 1, 0],
-    [0, 0, 1, 1, 1]
-])
+    if n > 1:
+        factors.append(n)
+    return factors
 
-# Funktion, um den Mindest-Hamming-Abstand basierend auf linear abhängigen Spalten zu berechnen
-def min_hamming_distance(H):
-    n = H.shape[1]  # Anzahl der Spalten
-    for r in range(2, n + 1):  # Anzahl der zu prüfenden Spaltenkombinationen
-        for cols in combinations(range(n), r):
-            submatrix = H[:, cols]
-            if np.linalg.matrix_rank(submatrix) < r:
-                return r
-    return n
 
-# Mindest-Hamming-Abstand berechnen
-d_min = min_hamming_distance(H)
-print(f"Der Mindest-Hamming-Abstand ist: {d_min}")
+if __name__ == '__main__':
+    N = 263713
+    factors = find_prime_factors(N)
+    print(factors)
+
